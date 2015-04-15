d941749ba650622bdd0db22a3375a7f950bf652e
Hayden Stainsby <hds@caffeineconcepts.com>
2015-04-15 18:39:57 +0200

    The pillar and blokk moving side to side

--- src/actors/pillar.js
+++ src/actors/pillar.js
@@ -1,9 +1,13 @@
 'use strict';
 
-pillar.Hero = cc.Class.extend({
+pillar.Pillar = cc.Class.extend({
     _sprite: null,
+    _light: null,
     _pos: cc.p(0, 0),
     _hitbox: cc.rect(0, 0, 0, 0),
+    _countdownStart: 5.0,
+    _countdown: 0.5,
+    _colour: 0,
 
     ctor: function()  {
         // this._super();
@@ -16,9 +20,27 @@ pillar.Hero = cc.Class.extend({
         this.initHitbox();
     },
 
+    update: function(delta)  {
+        this._countdown -= delta;
+
+        if (this._countdown <= 0)  {
+            this.switchLight();
+
+            this._countdown = this._countdownStart;
+            if (this._countdownStart > 0.2)
+                this._countdownStart -= 0.1;
+        }
+    },
+
     initSprite: function()  {
-        this._sprite = new cc.LayerColor(cc.color('#333333'), 64, 64);
+        this._sprite = new cc.LayerColor(cc.color('#999999'), 16, 64);
         this._sprite.setAnchorPoint(0.5, 0.5);
+
+        this._light = new cc.LayerColor(cc.color('#FFE10E'), 16, 8);
+        this._light.setPosition(cc.p(0, 48));
+        this._sprite.addChild(this._light);
+
+        this._countdown = this._countdownStart;
 
         this.updateSprite();
     },
@@ -31,12 +53,21 @@ pillar.Hero = cc.Class.extend({
         }
     },
 
+    switchLight: function()  {
+        if (this._colour === 0)
+            this._light.setColor(cc.color('#FB000D'));
+        else
+            this._light.setColor(cc.color('#FFE10E'));
+
+        this._colour = (this._colour + 1) % 2;
+    },
+
     getSprite: function()  {
         return this._sprite;
     },
 
     initHitbox: function()  {
-        this._hitbox = cc.rect(-32, -32, 64, 64);
+        this._hitbox = cc.rect(-16, -32, 32, 64);
     },
 
     getPosition: function()  {
--- src/levels/gamelevel.js
+++ src/levels/gamelevel.js
@@ -1,42 +1,112 @@
 'use strict';
 
 pillar.LevelLayer = cc.Layer.extend({
-    hero: null,
+    _hero: null,
+    _pillar: null,
+    _ground: 64,
+    _controls: {'right': false, 'left': false, 'jump': false, 'attack': false},
 
     ctor: function()  {
         this._super();
         this.init();
     },
 
+    // **** Init ****
     init: function()  {
         this._super();
+
+        // Initialise controls.
+        this.initControls();
 
         // Initialise the world
         this.initWorld();
 
-        // Initialise the hero.
+        // Initialise the pillar.
+        this.initPillar();
+
+        // Initialise Blokk.
         this.initHero();
 
+        this.scheduleUpdate();
 
         return true;
     },
 
     initWorld: function()  {
         var visibleSize = cc.winSize;
-        var background = new cc.LayerColor(cc.color('#2ab8ac'), visibleSize.width, visibleSize.height);
 
+        cc.log('Game world size:', visibleSize);
+
+        var background = new cc.LayerColor(cc.color('#B0ECF7'), visibleSize.width, visibleSize.height);
         this.addChild(background, 0);
+
+        var ground = new cc.LayerColor(cc.color('#4BC9C0'), visibleSize.width, this._ground);
+        this.addChild(ground, 1);
+    },
+
+    initPillar: function()  {
+        this._pillar = new pillar.Pillar();
+
+        this._pillar.setPosition(cc.p(cc.winSize.width/2.0, this._ground + this._pillar.getHitbox().height/2.0));
+        this.addChild(this._pillar.getSprite(), 20);
     },
 
     initHero: function()  {
-        this.hero = new pillar.Hero();
+        this._hero = new pillar.Hero();
 
-        cc.log(this.hero);
+        this._hero.setPosition(cc.p(300, this._ground + this._hero.getHitbox().height/2.0));
 
-        this.hero.setPosition(cc.p(100, 0));
+        this.addChild(this._hero.getSprite(), 10);
+    },
 
-        this.addChild(this.hero.sprite, 10);
+    initControls: function()  {
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed:  function(keyCode, event)  {
+                var layer = event.getCurrentTarget();
+                layer.setControlState(keyCode, true);
+                cc.log("Key " + keyCode.toString() + " was pressed!");
+            },
+            onKeyReleased: function(keyCode, event){
+                var layer = event.getCurrentTarget();
+                layer.setControlState(keyCode, false);
+            }
+        }, this);
+    },
+
+    // **** Update ****
+    update: function(delta)  {
+        this._pillar.update(delta);
+
+        var bpos = this._hero.getPosition();
+        if (this._controls.right)  {
+            bpos.x += 500 * delta;
+        }
+        else if (this._controls.left)  {
+            bpos.x -= 500 * delta;
+        }
+        this._hero.setPosition(bpos);
+
+        // cc.log(this._controls);
+    },
+
+    setControlState: function(e, state)  {
+        switch (e)  {
+            case cc.KEY.right:
+                this._controls.right = state;
+                break;
+            case cc.KEY.left:
+                this._controls.left = state;
+                break;
+            case cc.KEY.z:
+                this._controls.jump = state;
+                break;
+            case cc.KEY.x:
+                this._controls.attack = state;
+                break;
+        }
     }
+
 });
 
 pillar.LevelScene = cc.Scene.extend({
